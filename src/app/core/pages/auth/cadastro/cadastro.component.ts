c434f6fb209eeb25d50d3e6e61ecc6054154b307
DevRodrigoSantana <dev.rodrigosantana@hotmail.com>
2025-03-30 21:24:51 -0300

    feature: Adição de navbar e imagem

--- src/app/core/pages/auth/cadastro/cadastro.component.ts
+++ src/app/core/pages/auth/cadastro/cadastro.component.ts
@@ -1,10 +1,12 @@
+
+import { HeaderComponent } from '../../../share/header/header.component';
 import { FormCadastroComponent } from './../../../components/form-cadastro/form-cadastro.component';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [FormCadastroComponent],
+  imports: [FormCadastroComponent,HeaderComponent],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.scss',
 })
